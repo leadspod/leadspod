47d6308c96b3f19691159e095e373f4bfd690ed0
easydrain <jesse@accountshifter.com>
2019-04-16 17:42:27 +0200

    cc

--- plugin/leadspod/content.js
+++ plugin/leadspod/content.js
@@ -2,9 +2,8 @@
 var getDeleteButton = function(){
   var button = document.createElement("button");
   button.innerHTML = "Delete";
-  // 3. Add event handler
+  button.classList.add("delete-button");
   button.addEventListener ("click", function() {
-    console.log(this);
     this.parentElement.remove();
   });
   return button;
@@ -12,36 +11,24 @@ var getDeleteButton = function(){
 
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-
-  console.log(request);
-
-
   var posts = document.querySelectorAll('.feed-shared-update-v2');
-
   posts.forEach(function(element) {
-    console.log(element);
-    //feed-shared-update-v2__description
-
     var targetDiv = element.getElementsByClassName("feed-shared-update-v2__description")[0];
-    console.log(element.innerText);
-
     var re = new RegExp(request, 'gi')
     var matches = targetDiv.innerText.match(re)
     if(matches && matches.length > 0){
-      var deleteButton = getDeleteButton();
-      element.appendChild(deleteButton);
+      var button = element.getElementsByClassName("delete-button");
+      if(button.length == 0){
+        var deleteButton = getDeleteButton();
+        element.appendChild(deleteButton);
+        element.style.border = "thick solid red";
+      }
     }else{
       element.remove();
       window.scrollBy(0, 100);
       window.scrollBy(0, -100);
     }
-
-
-
   });
-
-
-   //sendResponse({count: matches.length})
 })
 
 
--- plugin/leadspod/popup.js
+++ plugin/leadspod/popup.js
@@ -7,12 +7,23 @@ document.addEventListener('DOMContentLoaded', function () {
   //   document.body.appendChild(div)
   // })
 
+console.log(chrome.storage);
+
+     chrome.storage.local.get(['key'], function(result) {
+       console.log('Value currently is ' + result.key);
+       if(result.key){
+         document.querySelector('#keyword').value = result.key;
+       }
+     });
+
+
   document.querySelector('button').addEventListener('click', onclick, false)
 
   function onclick () {
    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
      var keyword = document.querySelector('#keyword').value;
      chrome.tabs.sendMessage(tabs[0].id, keyword, null)
+     chrome.storage.local.set({key: keyword}, function() {});
    })
   }
 
