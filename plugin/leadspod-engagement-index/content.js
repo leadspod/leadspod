6566f6fa7e4175841216b67cf6043f14c9a446ba
easydrain <jesse@accountshifter.com>
2019-04-24 10:19:20 +0200

    cc

--- plugin/leadspod-engagement-index/content.js
+++ plugin/leadspod-engagement-index/content.js
@@ -1,3 +1,52 @@
+(function(win) {
+    'use strict';
+
+    var listeners = [],
+    doc = win.document,
+    MutationObserver = win.MutationObserver || win.WebKitMutationObserver,
+    observer;
+
+    function ready(selector, fn) {
+        // Store the selector and callback to be monitored
+        listeners.push({
+            selector: selector,
+            fn: fn
+        });
+        if (!observer) {
+            // Watch for changes in the document
+            observer = new MutationObserver(check);
+            observer.observe(doc.documentElement, {
+                childList: true,
+                subtree: true
+            });
+        }
+        // Check if the element is currently in the DOM
+        check();
+    }
+
+    function check() {
+        // Check the DOM for elements matching a stored selector
+        for (var i = 0, len = listeners.length, listener, elements; i < len; i++) {
+            listener = listeners[i];
+            // Query for elements matching the specified selector
+            elements = doc.querySelectorAll(listener.selector);
+            for (var j = 0, jLen = elements.length, element; j < jLen; j++) {
+                element = elements[j];
+                // Make sure the callback isn't invoked with the
+                // same element more than once
+                if (!element.ready) {
+                    element.ready = true;
+                    // Invoke the callback with the element
+                    listener.fn.call(element, element);
+                }
+            }
+        }
+    }
+
+    // Expose `ready`
+    win.ready = ready;
+
+})(this);
 var counter = {
   posts: 0,
   likes: 0,
@@ -62,13 +111,19 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     window.startRunning = true;
   }
 })
-var connectButton = document.getElementsByClassName("pv-s-profile-actions")[0];
-if(connectButton) {
-  console.log(location);
-  var newlink = document.createElement('a');
-  var linkText = document.createTextNode("POSTS");
-  newlink.appendChild(linkText);
-  newlink.setAttribute('href', location.href + 'detail/recent-activity/shares/');
-  connectButton.parentNode.parentNode.appendChild(newlink);
-  console.log(newlink);
+var state = {
+  currentUrl : ""
 }
+ready('.pv-s-profile-actions', function(element) {
+  var connectButton = document.getElementsByClassName("pv-s-profile-actions")[0];
+  if(connectButton) {
+    var newlink = document.createElement('a');
+    var linkText = document.createTextNode("POSTS");
+    newlink.appendChild(linkText);
+    if(state.currentUrl !== location.href + 'detail/recent-activity/shares/'){
+      state.currentUrl = location.href + 'detail/recent-activity/shares/';
+      newlink.setAttribute('href', state.currentUrl);
+      connectButton.parentNode.parentNode.appendChild(newlink);
+    }
+  }
+});
