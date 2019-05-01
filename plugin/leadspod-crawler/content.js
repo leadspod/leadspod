3b544ef1f45585dcf4b0909bd4033e1b1015ca6b
Jeina <jaina.vvr@gmail.com>
2019-05-01 22:35:18 +0300

    cc

--- plugin/leadspod-crawler/content.js
+++ plugin/leadspod-crawler/content.js
@@ -13,14 +13,22 @@ var getSocialCounts = function (child, kind) {
   var matches = child.innerText.match(re)
   if(matches && matches.length > 0) {
     var nr = child.innerText.split(' ')[0];
-    return parseInt(nr);
+    nr = nr.replace(/\,/g,'');
+    return parseInt(nr,10);
   }
   return 0;
+}
+
+var roundToTwo = function(num) {    
+    return +(Math.round(num + "e+2")  + "e-2");
 }
 
 var startIndex = function(){
   var posts = document.querySelectorAll('.feed-shared-update-v2');
   window.startRunning = true;
+  var followers = document.querySelectorAll('.pv-recent-activity-top-card__follower-count')[0].innerText.split(' ')[0];
+  followers = followers.replace(/\,/g,'');
+  var followersNumber=parseInt(followers,10);
 
   if(posts.length == 0) {
     shake();
@@ -33,7 +41,7 @@ var startIndex = function(){
     posts.forEach(function (element) {
       var timePeriodDiv = element.getElementsByClassName("feed-shared-actor__sub-description")[0];
       // console.log(timePeriodDiv.innerText);
-      console.log(counter.posts);
+      // console.log(counter.posts);
       var socialCounts = element.getElementsByClassName('feed-shared-social-counts')[0];
       if(socialCounts) {
         for(var i = 0; i < socialCounts.children.length; i++) {
@@ -43,10 +51,23 @@ var startIndex = function(){
       }
       // var re = new RegExp('gi')
       // var matches = timePeriodDiv.innerText.match(re)
-      if(counter.posts >= 6) {
+      if(counter.posts >= 10) {
         if(window.startRunning) {
           window.startRunning = false;
-          alert("posts : " + counter.posts + "\n" + "likes : " + counter.likes + "\n" + "comments : " + counter.comments + "");
+          // alert("posts : " + counter.posts + "\n" + "likes : " + counter.likes + "\n" + "comments : " + counter.comments + "");
+          
+          var averageComments = counter.comments / counter.posts;
+          var averageLikes = counter.likes / counter.posts;
+
+          var engagementIndex = ((counter.comments + counter.likes)/followersNumber)*100;
+
+          console.log("Posts: " + counter.posts);
+          console.log("Likes: " + counter.likes);
+          console.log("Comments: " + counter.comments);
+          console.log("Average comments per post: " + averageComments);
+          console.log("Average likes per post: " + averageLikes);
+          console.log("Engagement Index: " + roundToTwo(engagementIndex)+"%");
+
           //location.reload();
         }
       } else {
@@ -116,7 +137,7 @@ var start = function (request, sender, sendResponse) {
         if(matchesTimePeriod && matchesTimePeriod.length > 0) {
           if(window.startRunning) {
             window.startRunning = false;
-            if( counter.comments > 2 && counter.comments < 50 && counter.likes < 100 ) {
+            if( counter.comments < 10 && counter.likes < 20 ) {
               console.log("likes " + counter.likes);
               console.log("comments " + counter.comments);
               console.log(postsUrl);
