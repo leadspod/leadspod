c8e7bc63250e93c3c79eb1eff9e8363fb010b596
Robert Cooke <robertjessecooke@gmail.com>
2019-05-01 23:28:54 +0700

    cc

--- plugin/leadspod-crawler/content.js
+++ plugin/leadspod-crawler/content.js
@@ -1,48 +1,12 @@
-// var getPostIds = function () {
-//   var postIds = JSON.parse(localStorage.getItem("postids"));
-//   if(postIds == null || postIds == "") {
-//     postIds = [];
-//   }
-//   return postIds;
-// }
-// var addToPostIds = function (id) {
-//   var postIds = getPostIds();
-//   postIds.push(id);
-//   localStorage.setItem("postids", JSON.stringify(postIds));
-// }
-// var isInPostIds = function (id) {
-//   var postIds = getPostIds();
-//   if(postIds.indexOf(id) == -1) return false;
-//   return true;
-// }
-// var getDeleteButton = function () {
-//   var button = document.createElement("button");
-//   button.innerHTML = "Delete";
-//   button.style.position = "absolute";
-//   button.style.marginTop = "-21px";
-//   button.classList.add("delete-button");
-//   button.addEventListener("click", function () {
-//     var post = this.parentElement;
-//     var id = post.parentElement.getAttribute('data-id');
-//     addToPostIds(id);
-//     post.remove();
-//     window.scrollTo({
-//       top: 0,
-//       behavior: 'smooth'
-//     });
-//     window.startRunning = true;
-//   });
-//   return button;
-// }
-
-
 var shake = function () {
+  window.scrollTo(0, document.body.scrollHeight);
   window.scrollBy(0, 100);
   window.scrollBy(0, -100);
 }
 var counter = {
   likes: 0,
-  comments: 0
+  comments: 0,
+  posts: 0
 }
 var getSocialCounts = function (child, kind) {
   var re = new RegExp(kind, 'gi')
@@ -54,46 +18,60 @@ var getSocialCounts = function (child, kind) {
   return 0;
 }
 
+var startIndex = function(){
+  var posts = document.querySelectorAll('.feed-shared-update-v2');
+  window.startRunning = true;
+
+  if(posts.length == 0) {
+    shake();
+    setTimeout(function () {
+      // start(request, sender, sendResponse);
+      window.startRunning = true;
+    }, 2000);
+  } else {
+
+    posts.forEach(function (element) {
+      var timePeriodDiv = element.getElementsByClassName("feed-shared-actor__sub-description")[0];
+      // console.log(timePeriodDiv.innerText);
+      console.log(counter.posts);
+      var socialCounts = element.getElementsByClassName('feed-shared-social-counts')[0];
+      if(socialCounts) {
+        for(var i = 0; i < socialCounts.children.length; i++) {
+          counter.likes = counter.likes + getSocialCounts(socialCounts.children[i], 'likes');
+          counter.comments = counter.comments + getSocialCounts(socialCounts.children[i], 'comments');
+        }
+      }
+      // var re = new RegExp('gi')
+      // var matches = timePeriodDiv.innerText.match(re)
+      if(counter.posts >= 6) {
+        if(window.startRunning) {
+          //window.startRunning = false;
+          alert("posts : " + counter.posts + "\n" + "likes : " + counter.likes + "\n" + "comments : " + counter.comments + "");
+          //location.reload();
+        }
+      } else {
+        element.remove();
+        counter.posts++;
+      }
+    });
+  }
+}
 
 window.onload = function(e) {
   if(window.location.href.indexOf('recent-activity/shares') > 0) {
-    var posts = document.querySelectorAll('.feed-shared-update-v2');
-    window.startRunning = true;
 
-    if(posts.length == 0) {
-      shake();
-      setTimeout(function () {
-        // start(request, sender, sendResponse);
-        window.startRunning = true;
+
+    if(!window.startRunning) {
+      setInterval(function () {
+        if(window.startRunning) {
+          startIndex();
+        }
       }, 2000);
-    } else {
-      var postsCount = 0;
-      posts.forEach(function (element) {
-        var timePeriodDiv = element.getElementsByClassName("feed-shared-actor__sub-description")[0];
-        // console.log(timePeriodDiv.innerText);
-        console.log(postsCount);
-        var socialCounts = element.getElementsByClassName('feed-shared-social-counts')[0];
-        if(socialCounts) {
-          for(var i = 0; i < socialCounts.children.length; i++) {
-            counter.likes = counter.likes + getSocialCounts(socialCounts.children[i], 'likes');
-            counter.comments = counter.comments + getSocialCounts(socialCounts.children[i], 'comments');
-          }
-        }
-        // var re = new RegExp('gi')
-        // var matches = timePeriodDiv.innerText.match(re)
-        if(postsCount < 6) {
-          if(window.startRunning) {
-            window.startRunning = false;
-            alert("posts : " + counter.posts + "\n" + "likes : " + counter.likes + "\n" + "comments : " + counter.comments + "");
-            //location.reload();
-          }
-        } else {
-          element.remove();
-          counter.posts++;
-        }
-        postsCount++;
-      });
+      window.startRunning = true;
     }
+
+
+
   }
 }
 
@@ -119,7 +97,7 @@ var start = function (request, sender, sendResponse) {
       var timePeriodDiv = element.getElementsByClassName("feed-shared-actor__sub-description")[0];
       var personUrl = '';
       if(element.getElementsByClassName("feed-shared-actor__container-link")[0]) {
-        personUrl = element.getElementsByClassName("feed-shared-actor__container-link")[0].getAttribute("href").split('?')[0]; 
+        personUrl = element.getElementsByClassName("feed-shared-actor__container-link")[0].getAttribute("href").split('?')[0];
       }
       var postsUrl = personUrl + "/detail/recent-activity/shares/";
 
@@ -138,7 +116,7 @@ var start = function (request, sender, sendResponse) {
         if(matchesTimePeriod && matchesTimePeriod.length > 0) {
           if(window.startRunning) {
             window.startRunning = false;
-            if( counter.comments > 2 && counter.comments < 5 && counter.likes < 10 ) {
+            if( counter.comments > 2 && counter.comments < 50 && counter.likes < 100 ) {
               console.log("likes " + counter.likes);
               console.log("comments " + counter.comments);
               console.log(postsUrl);
