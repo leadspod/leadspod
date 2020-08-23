5dde86c021437c060b4690282a03c9111d27af24
Jesse <jesse@localhost.localdomain>
2020-08-24 00:49:43 +0700

    cc

--- tracker-site/script.js
+++ tracker-site/script.js
@@ -14,7 +14,7 @@ $header_top.find('a').on('click', function() {
 });
 
 $('#fullpage').fullpage({
-    sectionsColor: ['#e8ded2', '#ffffff', '#e3f6f5', '#bae8e8', '#e3f6f5', '#e8ded2'],
+    sectionsColor: ['#ffffff', '#ffffff', '#e3f6f5', '#bae8e8', '#e3f6f5', '#e8ded2'],
     sectionSelector: '.vertical-scrolling',
     navigation: true,
     slidesNavigation: true,
