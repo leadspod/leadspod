71f3b047516e21b8863aa154a03e76c39956d3c5
Jesse <jesse@localhost.localdomain>
2020-08-23 12:50:11 +0700

    cc

--- tracker-site/script.js
+++ tracker-site/script.js
@@ -3,31 +3,31 @@ var $header_top = $('.header-top');
 var $nav = $('nav');
 
 $header_top.find('a').on('click', function() {
-  $(this).parent().toggleClass('open-menu');
+    $(this).parent().toggleClass('open-menu');
 });
 
 $('#fullpage').fullpage({
-  sectionsColor: ['#3dcfa1', '#348899', '#ff8b20', '#ff5757', '#ffd03c'],
-  sectionSelector: '.vertical-scrolling',
-  navigation: true,
-  slidesNavigation: true,
-  controlArrows: false,
-  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
-  menu: '#menu',
+    sectionsColor: ['#e8ded2', '#ffffff', '#e3f6f5', '#bae8e8', '#e8ded2'],
+    sectionSelector: '.vertical-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    controlArrows: false,
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    menu: '#menu',
 
-  afterLoad: function(anchorLink, index) {
-    $header_top.css('background', 'rgba(0, 47, 77, .3)');
-    $nav.css('background', 'rgba(0, 47, 77, .25)');
-    if (index == 5) {
-        $('#fp-nav').hide();
-      }
-  },
+    afterLoad: function(anchorLink, index) {
+        $header_top.css('background', 'rgba(0, 47, 77, .3)');
+        $nav.css('background', 'rgba(0, 47, 77, .25)');
+        if (index == 5) {
+            $('#fp-nav').hide();
+        }
+    },
 
-  onLeave: function(index, nextIndex, direction) {
-    if(index == 5) {
-      $('#fp-nav').show();
-    }
-  },
+    onLeave: function(index, nextIndex, direction) {
+        if (index == 5) {
+            $('#fp-nav').show();
+        }
+    },
 
 
 });
